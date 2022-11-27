a18c5236e0df90dd38240c62b7e7d7c64422496f
wuchuheng <root@wuchuheng.com>
2022-11-28 01:01:45 +0800

    init

---
+++ src/main/java/com/wuchuheng/java/http_demo_java/HttpDemoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.wuchuheng.java.http_demo_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpDemoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpDemoJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wuchuheng/java/http_demo_java/HttpDemoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuchuheng.java.http_demo_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpDemoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
